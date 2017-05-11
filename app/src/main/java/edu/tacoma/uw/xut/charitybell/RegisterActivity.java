d390726f791b0805ce067093204c458bc1268d98
Adam Waldron <acw86@uw.edu>
2017-05-11 01:06:49 -0700

    Was able to get simple email and password authentication working with firebase.

---
+++ app/src/main/java/edu/tacoma/uw/xut/charitybell/RegisterActivity.java
@@ -0,0 +1,87 @@
+package edu.tacoma.uw.xut.charitybell;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private Button mRegisterButton;
+    private EditText mEmail, mPassword;
+    private FirebaseAuth mAuth;
+    private TextView mLoginText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+
+        mRegisterButton = (Button) findViewById(R.id.registerButton);
+        mEmail = (EditText) findViewById(R.id.emailRegister);
+        mPassword = (EditText) findViewById(R.id.passwordRegister);
+        mLoginText = (TextView) findViewById(R.id.loginTextView);
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //create user
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(RegisterActivity.this, "Account Created!", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(RegisterActivity.this, AlarmActivity.class));
+                                    finish();
+                                } else {
+                                    Toast.makeText(RegisterActivity.this, "Could not create account: " + task.getException().getMessage(),
+                                            Toast.LENGTH_LONG).show();
+                                }
+
+                            }
+                        });
+
+            }
+        });
+
+        mLoginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+}
