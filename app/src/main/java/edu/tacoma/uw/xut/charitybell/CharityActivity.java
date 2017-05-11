19554813b2b81c05c4ac31d8e167f35adafa7e2f
Thomas Xu <t.xu@temperednetworks.com>
2017-05-10 20:24:11 -0700

    alarm works

---
+++ app/src/main/java/edu/tacoma/uw/xut/charitybell/CharityActivity.java
@@ -0,0 +1,13 @@
+package edu.tacoma.uw.xut.charitybell;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CharityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charity);
+    }
+}
