caee09a292080ff45b056fe9b791da9fc92f7266
awaldro1 <Adam.Waldron1@T-Mobile.com>
2017-05-26 14:58:06 -0700

    Added vibration to the alert dialog and tested alarm delete/cancel feature.

--- app/src/main/java/edu/tacoma/uw/xut/charitybell/AlarmDialogActivity.java
+++ app/src/main/java/edu/tacoma/uw/xut/charitybell/AlarmDialogActivity.java
@@ -5,12 +5,14 @@ import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class AlarmDialogActivity extends AppCompatActivity {
     MediaPlayer mp;
+    Vibrator vibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,12 @@ public class AlarmDialogActivity extends AppCompatActivity {
         Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
         mp = MediaPlayer.create(getApplicationContext(), notification);
         mp.start();
+
+
+        vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+        long pattern[]={0,300,200,300,500};
+        vibrator.vibrate(pattern, 0);
+
         // set title
         alertDialogBuilder.setTitle("Alarm!");
 
@@ -35,6 +43,7 @@ public class AlarmDialogActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog,int id) {
                         Toast.makeText(AlarmDialogActivity.this, "Snoozed!", Toast.LENGTH_SHORT).show();
                         mp.stop();
+                        vibrator.cancel();
                         finish();
                     }
                 })
@@ -42,6 +51,7 @@ public class AlarmDialogActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int id) {
                         Toast.makeText(AlarmDialogActivity.this, "Alarm Canceled. Time to get up!", Toast.LENGTH_SHORT).show();
                         mp.stop();
+                        vibrator.cancel();
                         finish();
                     }
                 });
